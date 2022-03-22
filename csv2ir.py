6d701c0873003a865740a77fbce1ccd621cfb565
Alex <64880908+Spexivus@users.noreply.github.com>
2022-03-22 01:21:09 -0400

    Edited to be more user friendly

--- csv2ir.py
+++ csv2ir.py
@@ -1,9 +1,10 @@
-from dataclasses import replace
 import os
 import csv
+import time
+
 
 def main():
-    mainPath = os.getcwd() + "\csv2ir"
+    mainPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\csv2ir"
     csvPath = mainPath + "\csv"
     irPath = mainPath + "\ir"
 
@@ -12,12 +13,23 @@ def main():
         os.mkdir(mainPath)
         os.mkdir(csvPath)
         os.mkdir(irPath)
+        print(f"Created main directory ({mainPath})\n")
 
-    input("Place .csv files in the csv directory and press enter to continue...")
-
+    if input(f"Place .csv files in the ({csvPath}) directory. Should I open it for you? (y/n). \n") == "y":
+        os.startfile(csvPath)
+    input("Press enter when you are finished placing files. \n")
 
     os.chdir(csvPath)
-    csvfiles = os.listdir() #list of csv files
+    csvfiles = os.listdir()  # list of csv files
+
+    if len(csvfiles) == 0:
+        print("No csv files found. Exiting...")
+        time.sleep(2)
+        exit()
+
+    counter = 0
+    print("Converting files... \n")
+    start = time.time()
 
     for file in csvfiles:
         if file.endswith(".csv"):
@@ -27,12 +39,13 @@ def main():
                 next(csvFile)
                 filename = file.replace(".csv", ".ir")
                 os.chdir(irPath)
+                counter += 1
                 with open(filename, "w") as irFile:
                     irFile.write(f"Filetype: IR signals file\n")
                     irFile.write(f"Version: 1\n")
                     for row in csv_reader:
                         irFile.write(f"#\n")
-                        functionName = row[0].replace(" ","_")
+                        functionName = row[0].replace(" ", "_")
                         irFile.write(f"name: {functionName}\n")
                         irFile.write(f"type: parsed\n")
                         if row[1] == "NECx1" or row[1] == "NEC1":
@@ -40,7 +53,7 @@ def main():
                         if row[1] == "NECx2" or row[1] == "NEC2":
                             irFile.write(f"protocol: Samsung32\n")
                         # JVC : https://www.sbprojects.net/knowledge/ir/jvc.php
-                        #elif row[1] == "protocol_here":
+                        # elif row[1] == "protocol_here":
                         #    irFile.write(f"protocol: protocol_here\n")
                         else:
                             irFile.write(f"protocol: {row[1]}\n")
@@ -61,5 +74,12 @@ def main():
                         if len(cRow) == 1:
                             cRow = "0" + cRow
                         irFile.write(f"command: {cRow} 00 00 00\n")
+
+    finish = time.time()
+    print(f"Converted {counter} files in {finish-start} seconds ({counter/(finish-start)} files per second) \n")
+    print("Auto-quitting in 5 seconds... ")
+    time.sleep(5)
+    quit()
+
 
 main()
