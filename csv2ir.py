325835e059bb55ab138427ef439e041ba9a8c901
Aaron Mavrinac <mavrinac@gmail.com>
2022-05-12 13:26:09 -0400

    Move conversion into separate function.

--- csv2ir.py
+++ csv2ir.py
@@ -4,6 +4,37 @@ from re import sub
 import time
 
 
+def convert(csv_in, ir_out, protocol="NECext"):
+    with open(csv_in, newline="") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_file)
+        with open(ir_out, "w") as ir_file:
+            ir_file.write(f"Filetype: IR signals file\n")
+            ir_file.write(f"Version: 1\n")
+            for row in csv_reader:
+                ir_file.write(f"#\n")
+                function_name = row[0].replace(" ", "_")
+
+                ir_file.write(f"name: {function_name}\n")
+                ir_file.write(f"type: parsed\n")
+                ir_file.write(f"protocol: {protocol}\n")
+
+                device_id = (hex(int(row[2])))[2:].replace('x', '0').upper()
+                if len(device_id) == 1:
+                    device_id = "0" + device_id
+
+                subdevice_id = "00" if (row[3] == "-1") else (hex(int(row[3])))[2:].replace('x', '0').upper()
+                if len(subdevice_id) == 1:
+                    subdevice_id = "0" + subdevice_id
+
+                command = (hex(int(row[4])))[2:].upper()
+                if len(command) == 1:
+                    command = "0" + command
+
+                ir_file.write(f"address: {device_id} {subdevice_id} 00 00\n")
+                ir_file.write(f"command: {command} 00 00 00\n")
+
+
 def main():
     mainPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\csv2ir"
     csvPath = mainPath + "\csv"
@@ -24,6 +55,8 @@ def main():
     if input("\nWould you like to set a custom protocol? (y/n)\n") == "y":
         customProtocol = True
         protocol = str(input("\nEnter the protocol you would like to use \nCurrently supported protocols: NEC, NECext, Samsung32).\n"))
+    else:
+        protocol = "NECext"
 
     os.chdir(csvPath)
     csvfiles = os.listdir()  # list of csv files
@@ -39,49 +72,7 @@ def main():
 
     for file in csvfiles:
         if file.endswith(".csv"):
-            os.chdir(csvPath)
-            with open(file, newline='') as csvFile:
-                csv_reader = csv.reader(csvFile, delimiter=',')
-                next(csvFile)
-                filename = file.replace(".csv", ".ir")
-                os.chdir(irPath)
-                counter += 1
-                with open(filename, "w") as irFile:
-                    irFile.write(f"Filetype: IR signals file\n")
-                    irFile.write(f"Version: 1\n")
-                    for row in csv_reader:
-                        irFile.write(f"#\n")
-                        functionName = row[0].replace(" ", "_")
-
-                        if customProtocol == False:
-                            protocol = "NECext"
-
-                        irFile.write(f"name: {functionName}\n")                 # name
-                        irFile.write(f"type: parsed\n")                         # type
-
-                        irFile.write(f"protocol: {protocol}\n")
-
-                        deviceID = row[2]  # Device row
-                        deviceID = (hex(int(deviceID)))[2:].replace('x', '0').upper()
-
-                        subdeviceID = row[3] # Subdevice row
-                        subdeviceID = (hex(int(subdeviceID)))[2:].replace('x', '0').upper()
-
-                        command = (hex(int(row[4])))[2:].upper()  # Command row (in hex)
-
-                        if row[3] == "-1":    # Check value before conversion
-                            subdeviceID = "00"
-
-                        if len(deviceID) == 1:
-                            deviceID = "0" + deviceID
-                        if len(subdeviceID) == 1:
-                            subdeviceID = "0" + subdeviceID
-
-                        irFile.write(f"address: {deviceID} {subdeviceID} 00 00\n")
-
-                        if len(command) == 1:
-                            command = "0" + command
-                        irFile.write(f"command: {command} 00 00 00\n")
+            convert(os.path.join(csvPath, file), os.path.join(irPath, file.replace(".csv", ".ir")), protocol)
 
     finish = time.time()-start
     print(f"\nConverted {counter} files in {finish} seconds ({counter/(finish)} files per second)")
